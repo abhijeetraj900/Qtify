ac85b1b9407fa0b80147030bc085a20c50d84291
Abhijeet Raj <abhijeetraj900@gmail.com>
2024-11-28 20:37:34 +0530

    refactor footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,12 +7,17 @@ function Footer() {
     <div className={styles.footerWrapper}>
       <div className={styles.imgCard}>
         <div className={styles.img}>
-          <img src={image} alt="" />
+          <img src={image} alt="Song artwork" />
         </div>
-        <div>
+        <div className={styles.textWrapper}>
           <p className={styles.songName}>Song Name</p>
-          <p className={styles.albumName}> Album Name</p>
+          <p className={styles.albumName}>Album Name</p>
         </div>
+      </div>
+      <div className={styles.footerText}>
+        <p>
+          Designed by <span className={styles.highlight}>Abhijeet Raj</span> with Love ❤️
+        </p>
       </div>
     </div>
   );
